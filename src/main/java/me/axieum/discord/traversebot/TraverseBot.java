c95e90d8fac2a31f9fc7f331bc1f048f2a3a8955
Axieum <7577851+Axieum@users.noreply.github.com>
2019-11-24 14:09:06 +1000

    Initial commit

---
+++ src/main/java/me/axieum/discord/traversebot/TraverseBot.java
@@ -0,0 +1,14 @@
+package me.axieum.discord.traversebot;
+
+public class TraverseBot
+{
+    /**
+     * TraverseBot main entry point.
+     *
+     * @param args system arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
