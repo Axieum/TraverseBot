d40161d2d6d34dce4d12de956015260d0e2136c4
Axieum <7577851+Axieum@users.noreply.github.com>
2019-11-27 22:48:04 +1000

    Added 'raw' toggle to IP command

--- src/main/java/me/axieum/discord/traversebot/command/CommandIP.java
+++ src/main/java/me/axieum/discord/traversebot/command/CommandIP.java
@@ -14,6 +14,7 @@ public class CommandIP extends Command
     public CommandIP()
     {
         this.name = "ip";
+        this.arguments = "[raw]";
         this.help = "Provides the server's IP address";
     }
 
@@ -24,7 +25,7 @@ public class CommandIP extends Command
         String ip = Config.getConfig().get("ip");
 
         // If no ip configured, then fetch public IP
-        if (ip == null || ip.isEmpty()) {
+        if (event.getArgs().equalsIgnoreCase("raw") || ip == null || ip.isEmpty()) {
             try {
                 event.getChannel().sendTyping().queue(); // Let them know we're fetching the IP
                 ip = new BufferedReader(new InputStreamReader(new URL("http://checkip.amazonaws.com").openStream()))
